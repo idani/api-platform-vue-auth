dacd0b1cb050dbf6044766540fe90204d043d19d
Shigehiro IDANI <idani@hirotae.com>
2020-09-07 18:42:26 +0900

    Vue-routerのパスとSymfonyのルーターのパスを設定する

--- src/src/Controller/AppController.php
+++ src/src/Controller/AppController.php
@@ -8,9 +8,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class AppController extends AbstractController
 {
     /**
-     * @Route("/app", name="app")
+     * @Route("/", name="top")
      */
     public function index()
+    {
+        return $this->render('app/index.html.twig', [
+            'controller_name' => 'AppController',
+        ]);
+    }
+
+    /**
+     * @Route("/products/{subdir?}", name="products")
+     */
+    public function products()
     {
         return $this->render('app/index.html.twig', [
             'controller_name' => 'AppController',
